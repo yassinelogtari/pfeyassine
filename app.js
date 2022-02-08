653e2373902d9abd1c30e64f9a0b49bab8e7b882
yassinelogtari <60779968+yassinelogtari@users.noreply.github.com>
2022-02-08 16:51:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.listen(process.env.PORT, function(){
+    console.log("server is running on port 3000");
+});
